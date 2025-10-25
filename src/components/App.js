ed33c2f4abf5f50e6e938d0f099f01b163e126ec
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-25 23:21:40 +0000

    updated and added files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import Home from './HomePage'
+import About from './AboutPage'
+import Navigation from './Navigation'
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path='/' element={<Home />} ></Route>
+          <Route path='/about' element={<About />} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
