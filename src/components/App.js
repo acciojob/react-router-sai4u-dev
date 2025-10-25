41c8ad5402530b45e525669897dbeb29e1e9b49d
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-25 23:24:17 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 
 import React from "react";
 import './../styles/App.css';
-import Home from './HomePage'
-import About from './AboutPage'
+import Home from './Home'
+import About from './About'
 import Navigation from './Navigation'
 
 const App = () => {
