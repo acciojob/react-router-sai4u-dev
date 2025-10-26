6334994ee55f9a2c131a871b0b432260c9bcf553
Sai Narendra <tannirusainarendra81@gmail.com>
2025-10-26 00:17:29 +0000

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,19 @@
-
 import React from "react";
-import './../styles/App.css';
-import Home from './Home'
-import About from './About'
-import Navigation from './Navigation'
+import Home from './Home';
+import About from './About';
+import Navigation from './Navigation';
+import { Route, Switch } from "react-router-dom"; // v5 uses Switch
 
 const App = () => {
   return (
     <div>
-      {/* Do not remove the main div */}
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path='/' element={<Home />} ></Route>
-          <Route path='/about' element={<About />} ></Route>
-        </Routes>
-      </BrowserRouter>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
